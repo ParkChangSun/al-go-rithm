5c1727a770e93ab3c9459563e092ecacfe1b9357
ParkChangSun <changsun19991212@gmail.com>
2025-11-01 23:50:02 +0900

    baek1467

--- baek1467/main.go
+++ baek1467/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"maps"
 	"os"
 	"slices"
 )
@@ -12,38 +13,53 @@ var std = bufio.NewReadWriter(bufio.NewReader(os.Stdin), bufio.NewWriter(os.Stdo
 func main() {
 	defer std.Flush()
 
-	var n, targets []byte
-	fmt.Fscan(std, &n, &targets)
+	var numbers, targets []byte
+	fmt.Fscan(std, &numbers, &targets)
 
-	for _, target := range targets {
-		hold := -1
-		for i, number := range n {
-			if target == number {
-				if i == len(n)-1 {
+	reduced := map[byte]int{}
+	for _, v := range numbers {
+		reduced[v]++
+	}
+	for _, v := range targets {
+		reduced[v]--
+		if reduced[v] == 0 {
+			delete(reduced, v)
+		}
+	}
 
-					hold = i
+	res := []byte{}
 
-					break
-				}
+	for len(reduced) > 0 {
+		for i := byte('9'); i > byte('0'); i-- {
+			if _, o := reduced[i]; !o {
+				continue
+			}
+			targetIndex := slices.Index(numbers, i)
 
-				if n[i+1] < target {
-
-					hold = i
-
-				} else if n[i+1] == target {
-					if hold == -1 {
-						hold = i
-					}
-				} else {
-					hold = i
+			contains := true
+			temp := maps.Clone(reduced)
+			for _, v := range numbers[targetIndex:] {
+				temp[v]--
+			}
+			for _, v := range temp {
+				if v > 0 {
+					contains = false
 					break
 				}
 			}
-		}
+			if !contains {
+				continue
+			}
 
-		n = slices.Delete(n, hold, hold+1)
-		// fmt.Println(string(target), string(n))
+			res = append(res, i)
+			numbers = numbers[targetIndex+1:]
+			reduced[i]--
+			if reduced[i] == 0 {
+				delete(reduced, i)
+			}
+			break
+		}
 	}
 
-	fmt.Fprint(std, string(n))
+	fmt.Fprint(std, string(res))
 }
